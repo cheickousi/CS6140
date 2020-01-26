39ccc75ba02999a81a5f8266d1f84ad1df907e5b
Cheick Sissoko <cheick.sissoko@gmail.com>
2020-01-26 16:57:11 -0700

    homework2 second section done

--- homework2/min_hashing.py
+++ homework2/min_hashing.py
@@ -1,7 +1,9 @@
 import numpy as np
 from string import punctuation
 import pandas as pd
-# np.random.seed(2)
+import time
+
+np.random.seed(2)
 from homework2.inputs import D1, D2
 
 
@@ -19,15 +21,14 @@ def get_min_hash_signature(matrix, my_hash):
     pd_vector = pd.DataFrame([matrix[my_hash].iloc[0]], columns=my_hash)
     updated_domain = matrix[my_hash]
     for index, row in updated_domain.iterrows():
-        pd_vector = row.where(pd_vector.iloc[0] > row[my_hash], pd_vector.iloc[0])
-    return pd_vector.values
+        pd_vector = row.where(row[my_hash] < pd_vector.iloc[0], row[my_hash])
+    return pd_vector
 
 
-def get_domain(text, text2, k_grams, hash_funct):
-    a_hash = split_char(k_grams, text)
-    b_hash = split_char(k_grams, text2)
+def get_domain(a_hash, b_hash, hash_funct):
     domain = np.unique(np.hstack([a_hash, b_hash]))
-    hash_function_mapping = np.random.randint(low=1, high=np.power(domain.shape[0], 2), size=[hash_funct, domain.shape[0]])
+    hash_function_mapping = np.random.randint(low=1, high=np.power(domain.shape[0], 2),
+                                              size=[hash_funct, domain.shape[0]])
     domain_matrix = pd.DataFrame([], columns=domain)
     for each_row in hash_function_mapping:
         new_row = pd.DataFrame([each_row], columns=domain)
@@ -38,11 +39,24 @@ def get_domain(text, text2, k_grams, hash_funct):
 # generating permuation functions
 
 
-number_hash_functions = [20, 60, 150, 300, 600]
+number_hash_functions = [10,20, 60, 150, 300, 600]
 k_grams = 3
+
+headers = ["t", "JS(D1, D2)", " Time Elapsed"]
+report = pd.DataFrame([], columns=headers)
+a_hash = split_char(k_grams, D1)
+b_hash = split_char(k_grams, D2)
 for each_hash_q in number_hash_functions:
-    my_domain = get_domain(D1, D2, k_grams, each_hash_q)
-    a_sign = get_min_hash_signature(my_domain, split_char(k_grams, D1))
-    b_sign = get_min_hash_signature(my_domain, split_char(k_grams, D2))
+    startTime = time.time()
+    my_domain = get_domain(a_hash, b_hash, each_hash_q)
+    a_sign = get_min_hash_signature(my_domain, a_hash)
+    b_sign = get_min_hash_signature(my_domain, b_hash)
+    total_k = my_domain.shape[1]
+    intersection = np.intersect1d(a_sign, b_sign).__len__()
+    jacc = intersection / total_k
+    endTime = time.time()
+    timeElapsed1 = endTime - startTime
+    rep = pd.DataFrame([[each_hash_q, jacc, timeElapsed1]], columns=headers)
+    report = report.append(rep)
 
-
+print(report)
