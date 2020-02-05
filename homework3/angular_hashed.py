c58057f20334415e630e43ed54580f3883e92138
Cheick Sissoko <cheick.sissoko@gmail.com>
2020-02-05 07:04:11 -0700

    homework3 final draft

--- homework3/angular_hashed.py
+++ homework3/angular_hashed.py
@@ -3,23 +3,40 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
+def generate_angular_report(list_of_pairs, data):
+    header = ["Pairs", "Angular Similarity"]
+    report = pd.DataFrame([], columns=header)
+    number_above = 0
+    for each_pair in list_of_pairs:
+        key1 = each_pair[0]
+        key2 = each_pair[1]
+        dot_prod = np.dot(data.get(key1), data.get(key2))
+        similarity = 1 - (1 / np.pi) * np.arccos(dot_prod)
+        rep = pd.DataFrame([[each_pair, similarity]], columns=header)
+        report = report.append(rep)
+        if float(similarity) >= float(0.85):
+            number_above += 1
+    hist = report.hist(bins=len(list_of_pairs) * 2, cumulative=True, density=True)
+    return hist, number_above
+
+
 directory = "//Users/CheickSissoko/Documents/Spring2020_CS6140/homework3/R.csv"
 
 n = 500
 mylist = np.array(range(n)) + 1
 list_of_pairs = list(combinations(mylist, 2))
-file =pd.read_csv(directory, header=None).T
+file = pd.read_csv(directory, header=None).T
 file.columns = mylist
 
-header = ["a,b", "dot Product"]
 
-report = pd.DataFrame([], columns=header)
-for each_pair in list_of_pairs:
-    key1 = each_pair[0]
-    key2 = each_pair[1]
-    prod = np.dot(file.get(key1), file.get(key2))
-    rep = pd.DataFrame([[each_pair, prod]], columns=header)
-    report = report.append(rep)
+# historigram, number_above = generate_angular_report(list_of_pairs, file)
+# print(number_above)
 
-hist = report.hist(bins=len(list_of_pairs) * 2, cumulative=True, density=True)
+from homework3.random_direction import list_of_pairs as list_pairs_B, myList_vectors
+
+historigram2, number_above2 = generate_angular_report(list_pairs_B, myList_vectors)
+
+print(number_above2)
+
 plt.show()
--- homework3/lsh.py
+++ homework3/lsh.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-from scipy.interpolate import splrep, splev
+from scipy.interpolate import splrep, splev, interp1d
 
 file = "//Users/CheickSissoko/Documents/Spring2020_CS6140/homework3/R.csv"
 
@@ -23,20 +23,38 @@ def get_report(s, b, r):
 
 
 t = 160
-r = 85
-b = t / r
-r2 = np.power(1 / r, 1 / b)
-b2 = - math.log(t, r2)
-s = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+r = np.arange(5, 100, 15).tolist()
 
-b_report = get_report(s, b, r)
-b2_report = get_report(s, b2, r2)
+
+header = ["b", "r"]
+report = pd.DataFrame([], columns=header )
+s2 = np.arange(0.01, 1, 0.01).tolist()
+for each_r in r:
+    b = t / each_r
+    b_report = get_report(s2, b, each_r)
+    x = b_report["s"]
+    y = b_report["f(s)"]
+    x_new = np.linspace(x.min(), x.max(), 500)
+    f = interp1d(x, y, kind='quadratic')
+    y_smooth = f(x_new)
+    plt.plot(x_new, y_smooth, label = str(each_r))
+    plt.legend(loc="best")
+    rep = pd.DataFrame([[b, each_r]], columns=header)
+    report = report.append(rep)
+
+print(report)
+# plt.show()
+
+best_b = 8
+best_r = 20
+pairs = {"AB": 0.77, "AC": 0.25, "AD": 0.33, "BC": 0.20, "BD": 0.55, "DC": 0.91}
+
+headers = ["Pair", " Similarity", "Probability", "best b" ,"best r"]
+b_report = pd.DataFrame([], columns=headers )
+for key, each_p in pairs.items():
+        fs = 1 - math.pow(1 - math.pow(each_p, best_b), best_r)
+        reep = pd.DataFrame([[key, each_p, fs, best_b, best_r]], columns= headers)
+        b_report = b_report.append(reep)
+    # print(np.round(fs, 3))
+
 print(b_report)
-
-x = b_report["s"]
-y = b_report["f(s)"]
-print(b2_report)
-plt.figure()
-plt.plot(x, y)
-b2_report.plot(kind = "line", x="s",y="f(s)",color="red" )
-plt.show()
