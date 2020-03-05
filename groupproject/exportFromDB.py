4bfd0b698c8e2771a1bf5b8c28d0d3b31e604a4f
Cheick Sissoko <cheick.sissoko@gmail.com>
2020-03-04 21:08:22 -0700

    adding group project first draft

---
+++ groupproject/exportFromDB.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+import pandas as pd
+
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+           specified by db_file
+       :param db_file: database file
+       :return: Connection object or None
+       """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except sqlite3.Error as e:
+        print(e)
+
+    return conn
+
+
+directory = "/Users/CheickSissoko/Documents/SQLite/"
+local_filename = "/Users/CheickSissoko/Documents/Spring2020_CS6140/groupproject/first_draft_injuries.csv"
+
+conn = create_connection('/Users/CheickSissoko/Documents/SQLite/nba_injuries.db3')
+
+sql = 'SELECT * FROM INJURY_DATA WHERE PLAYER_EFFICIENCY_RATING IS NOT NULL '
+excel = pd.read_sql(sql=sql, con=conn)
+
+# excel.to_csv(path_or_buf="/Users/CheickSissoko/Documents/Spring2020_CS6140/groupproject/consolidateData.csv")
--- homework4/gonzalez.py
+++ homework4/gonzalez.py
@@ -1,14 +1,13 @@
-from itertools import combinations
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
-from numpy.linalg import norm
 from scipy.spatial.distance import euclidean
 
 delimiter = "\s+"
 directory = "/Users/CheickSissoko/Documents/CS6140/homework4/"
 C2 = pd.read_csv(directory + "C2.txt", delimiter=delimiter, header=None)
+
 
 # print(C2)
 
@@ -19,20 +18,23 @@ for rows in C2.itertuples():
     data_points.update({index: (rows._2, rows._3)})
     index += 1
 
+# print(data_points)
+
+data_points2 = {1: (0, 0), 2: (0, 7), 3: (3, 1), 4: (10, 0)}
 k = 3
-centers = {1: data_points.get(1)}
-array_ofj = np.random.uniform(1, 1, size=len(data_points.keys()))
+centers = {1: data_points2.get(1)}
+array_ofj = np.random.uniform(1, 1, size=len(data_points2.keys()))
 index = 2
 for _ in range(k - 1):
     m = 0
-    centers[index] = data_points.get(1)
-    for key, points in data_points.items():
+    centers[index] = data_points2.get(1)
+    for key, points in data_points2.items():
         current_center = centers.get(array_ofj[key-1])
         distance = euclidean (points, current_center)
         if distance > m:
             m = distance
             centers[index] = points
-    for key, points in data_points.items():
+    for key, points in data_points2.items():
         current_center = centers.get(array_ofj[key - 1])
         distance_j = euclidean(points, current_center)
         distance_i = euclidean(points, centers[index])
@@ -44,7 +46,7 @@ print(centers)
 
 headers =  ['Cluster_Group', "Center_Point", "X_values", "Y_values"]
 gonzalez_report = pd.DataFrame([],columns=headers)
-for key, value in data_points.items():
+for key, value in data_points2.items():
     center = array_ofj[key-1]
     value =  list(value)
     rep = pd.DataFrame([["Cluster_Group" + str(center), centers.get(center), value[0], value[1] ]], columns=headers)
@@ -54,4 +56,4 @@ sns.scatterplot(data=gonzalez_report, x='X_values', y='Y_values', hue='Cluster_G
 
 # print(gonzalez_report["Center_Group", "Center_Point"].groupby(by='Center_Point'))
 plt.title("Gonzalez")
-# plt.show()
+plt.show()
